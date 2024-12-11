d68651a42e8a2b84ca1d232c017ff6615ce50832
miachen0413 <spispil332@gmail.com>
2024-12-11 14:40:12 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
-require("dotenv").config();
+// require("dotenv").config();
 
+// const express = require('express');
+// const app = express()
+// const {
+//   neon
+// } = require("@neondatabase/serverless");
+// const sql = neon(process.env.DATABASE_URL);
+// const port = 3000
+
+// const requestHandler = async (req, res) => {
+//   const result = await sql `SELECT version()`;
+//   const {
+//     version
+//   } = result[0];
+//   res.writeHead(200, {
+//     "Content-Type": "text/plain"
+//   });
+//   res.end(version);
+//   return res.status(200).json({
+//     version
+//   })
+// };
+
+// app.get('/', requestHandler)
+// app.get('/bye', (req, res) => {
+//   res.send('bye')
+// })
 const express = require('express');
-const app = express()
-const {
-  neon
-} = require("@neondatabase/serverless");
-const sql = neon(process.env.DATABASE_URL);
-const port = 3000
-
-const requestHandler = async (req, res) => {
-  const result = await sql `SELECT version()`;
-  const {
-    version
-  } = result[0];
-  res.writeHead(200, {
-    "Content-Type": "text/plain"
-  });
-  res.end(version);
-  return res.status(200).json({
-    version
-  })
-};
-
-app.get('/', requestHandler)
-app.get('/bye', (req, res) => {
-  res.send('bye')
+const app = express();
+app.get('/', (req, res) => {
+  return res.send("Hello express")
 })
+app.listen(3000, () => console.log('Server ready on port 3000.'));
 
-app.listen(port, () => console.log('Server ready on port 3000.'));
-
-module.exports = app;
+module.export = app
